df09a4bcd2ed8b8a88102675b436bd3fcb75c67f
WizarPoka <Vasya-nve12401@yandex.ru>
2024-05-10 18:24:41 +0300

    хороший вывод расписания для отдельных групп, добавлены стили шрифта golos

--- client/src/GroupSchedule.jsx
+++ client/src/GroupSchedule.jsx
@@ -6,12 +6,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useParams } from 'react-router-dom';
 
+
+import './fonts/Golos_Text/GolosText-Regular.ttf'
+import styles from './GroupSchedule.module.css';
+
 // ====================================================================================
 
 const GroupSchedule = () => {
   const { group } = useParams();
   const [schedule, setSchedule] = useState(null);
-
+  
 // ====================================================================================
 
   useEffect(() => {
@@ -25,31 +29,64 @@ const GroupSchedule = () => {
     };
 
     fetchSchedule();
-  }, [group]);
+  }, [group], console.log(group));
 
 // ====================================================================================
 
-return (
-  <div>
-    <h1>Расписание</h1>
-    <Link to="/groups">Назад</Link>
-    <h2>Расписание для - {group}</h2>
-    {schedule ? (
-      <div>
-        <h3>Первая неделя</h3>
-        <pre>{JSON.stringify(schedule['Первая неделя'], null, 2)}</pre>
-        {schedule['Вторая неделя'] && (
-          <div>
-            <h3>Вторая неделя</h3>
-            <pre>{JSON.stringify(schedule['Вторая неделя'], null, 2)}</pre>
-          </div>
-        )}
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Расписание</h1>
+        <Link to="/groups">Назад</Link>
       </div>
-    ) : (
-      <p>Loading...</p>
-    )}
-  </div>
-);
+      <h2>{group}</h2>
+      {schedule ? (
+        <div>
+          {schedule.map((weekSchedule, index) => (
+            <div key={index} className={styles.week}>
+
+              <div className={styles.weekTitle}>
+                {index === 0 ? 'Первая неделя' : 'Вторая неделя'}
+              </div>
+              {weekSchedule.map((daySchedule, dayIndex) => (
+
+                <div key={dayIndex} className={styles.day}>
+
+                  <span className={styles.dayNumber}>
+                    {daySchedule.day}
+                  </span>
+
+                  <div>
+                    {daySchedule.lessons.map((lesson, lessonIndex) => (
+                      <li key={lessonIndex} className={styles.lessonTitle}>
+
+                        
+
+                        <span className={styles.number}>
+                          {lesson.number}
+                        </span>
+                        <span className={styles.lesson}>
+                          {lesson.lesson}
+                        </span>
+                        <span className={styles.teacher}>
+                          {lesson.teacher}
+                        </span>
+                        <span className={styles.classroom}>
+                          {lesson.classroom}
+                        </span>
+                      </li>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 
--- client/src/ListGroups.jsx
+++ client/src/ListGroups.jsx
@@ -1,5 +1,11 @@
+// ====================================================================================
+
+// ListGroups.jsx
+
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+// ====================================================================================
 
 const ListGroups = ({ schedule }) => {
   console.log(schedule);
@@ -7,25 +13,48 @@ const ListGroups = ({ schedule }) => {
     return <div>Loading...</div>;
   }
 
+// ====================================================================================
+  // Создаем список для хранения всех групп
+  const allGroups = [];
+
+  // Проходимся по каждой неделе в расписании
+  schedule.forEach(weekData => {
+    // Проходимся по каждой группе в текущей неделе
+    weekData.groups.forEach(groupData => {
+      // Проверяем, нет ли уже такой группы в списке allGroups
+      const existingGroup = allGroups.find(group => group.group === groupData.group);
+      // Если такой группы нет в списке, добавляем её
+      if (!existingGroup) {
+        allGroups.push(groupData);
+        
+      }
+    });
+  });
+  console.log(allGroups)
+
+// ====================================================================================
+
   return (
     <div>
-      <h2>Список групп</h2>
-      {schedule.map((weekData, weekIndex) => (
-        <div key={weekIndex}>
-          <h2>{weekData.week}</h2>
-          {weekData.groups.map((groupData, groupIndex) => (
-            
-              
-              
-              <Link to={`/schedule/group/${groupData.group}`}>
-                <button>{groupData.group}</button>
-              </Link>
-            
-          ))}
+
+      <h2>Список групп:</h2>
+
+      {allGroups.map((groupData, index) => (
+
+        <div key={index}>
+
+          <Link to={`/schedule/group/${groupData.group}`}>
+            <button>{groupData.group}</button>
+          </Link>
+
         </div>
       ))}
+
     </div>
   );
 };
+// ====================================================================================
 
 export default ListGroups;
+
+// ====================================================================================
